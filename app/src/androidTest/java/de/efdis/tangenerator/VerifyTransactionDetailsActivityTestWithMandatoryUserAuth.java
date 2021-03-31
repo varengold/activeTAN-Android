68d4a4b1e442eaf4744320d5b8a329b53a9ef39a
EFDIS AG Bankensoftware <info@efdis.de>
2021-03-31 12:44:22 +0200

    google-play-30

--- app/src/androidTest/java/de/efdis/tangenerator/VerifyTransactionDetailsActivityTestWithMandatoryUserAuth.java
+++ app/src/androidTest/java/de/efdis/tangenerator/VerifyTransactionDetailsActivityTestWithMandatoryUserAuth.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 EFDIS AG Bankensoftware, Freising <info@efdis.de>.
+ *
+ * This file is part of the activeTAN app for Android.
+ *
+ * The activeTAN app is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The activeTAN app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the activeTAN app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.efdis.tangenerator;
 
 import androidx.test.espresso.Espresso;
--- app/src/main/java/de/efdis/tangenerator/api/BankingAppApi.java
+++ app/src/main/java/de/efdis/tangenerator/api/BankingAppApi.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 EFDIS AG Bankensoftware, Freising <info@efdis.de>.
+ *
+ * This file is part of the activeTAN app for Android.
+ *
+ * The activeTAN app is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The activeTAN app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the activeTAN app.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.efdis.tangenerator.api;
 
 import android.app.Activity;
@@ -9,7 +28,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.view.View;
